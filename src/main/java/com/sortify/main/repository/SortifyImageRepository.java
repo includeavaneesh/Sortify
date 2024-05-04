789766647f5f437a6b2130321960dcd169e40827
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-05-04 15:10:17 -0400

    Fixed @repository annotation for Image repository

--- src/main/java/com/sortify/main/repository/SortifyImageRepository.java
+++ src/main/java/com/sortify/main/repository/SortifyImageRepository.java
@@ -2,6 +2,8 @@ package com.sortify.main.repository;
 
 import com.sortify.main.model.SortifyImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SortifyImageRepository extends JpaRepository<SortifyImage, String> {
 }
