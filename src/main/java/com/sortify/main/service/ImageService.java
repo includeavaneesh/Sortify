c493e950a825c3aea9b0754d0f5a5885ea51af6f
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-05-04 15:15:33 -0400

    Added service layer of image management

---
+++ src/main/java/com/sortify/main/service/ImageService.java
@@ -0,0 +1,31 @@
+package com.sortify.main.service;
+
+import com.sortify.main.model.SortifyImage;
+import com.sortify.main.repository.SortifyImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService implements SortifyImageService{
+
+    @Autowired
+    private SortifyImageRepository imageRepository;
+    @Override
+    public void saveImage(SortifyImage image) {
+        imageRepository.save(image);
+    }
+
+    @Override
+    public void deleteImage(String imageId) {
+        imageRepository.deleteById(imageId);
+    }
+
+    @Override
+    public SortifyImage findImage(String imageId) {
+        if(imageRepository.findById(imageId).isPresent()) {
+            return imageRepository.findById(imageId).get();
+        }
+
+        return null;
+    }
+}
