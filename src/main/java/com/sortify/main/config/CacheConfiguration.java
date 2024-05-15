3cdb4cd7ae7786937cf1816332f585ce14f573f6
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-05-14 20:53:27 -0400

    Updated cache configuration

--- src/main/java/com/sortify/main/config/CacheConfiguration.java
+++ src/main/java/com/sortify/main/config/CacheConfiguration.java
@@ -1,6 +1,7 @@
 package com.sortify.main.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
@@ -14,14 +15,22 @@ import java.util.concurrent.TimeUnit;
 @EnableCaching
 public class CacheConfiguration {
 
+    @Value("${caffeine.cache.min-size}")
+    private int MIN_SIZE;
+
+    @Value("${caffeine.cache.max-size}")
+    private int MAX_SIZE;
+
+    @Value("${caffeine.cache.time-to-live}")
+    private int TTL;
     @Bean
     @Primary
     public CacheManager cacheManager () {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("user_cache");
         cacheManager.setCaffeine(Caffeine.newBuilder()
-                .initialCapacity(1)
-                .expireAfterAccess(5, TimeUnit.SECONDS)
-                .maximumSize(2));
+                .initialCapacity(MIN_SIZE)
+                .expireAfterAccess(TTL, TimeUnit.MINUTES)
+                .maximumSize(MAX_SIZE));
         return cacheManager;
     }
 }
